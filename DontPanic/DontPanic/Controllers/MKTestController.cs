3254f59bc7b1a4ce5d61af137f25857a7556f934
Martin Kearn <martin.kearn@microsoft.com>
2015-10-22 17:25:07 +0100

    Add MKTest controller for playing with the data logic

---
+++ DontPanic/DontPanic/Controllers/MKTestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Web.Hosting;
+
+namespace DontPanic.Controllers
+{
+    public class MKTestController : Controller
+    {
+        // GET: MKTest
+        public ActionResult Index()
+        {
+            using (StreamReader sr = new StreamReader(Server.MapPath("~/mockEventsResponse.json")))
+            {
+                string content = sr.ReadToEnd();
+            }
+
+            return View();
+        }
+    }
+}
